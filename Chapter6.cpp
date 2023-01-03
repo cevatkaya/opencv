2e25769b8c0accf3fd4f12e03060f245cbf4e607
Cevat Kaya <48681093+cevatkaya@users.noreply.github.com>
2023-01-03 23:36:13 +0300

    Add files via upload

---
+++ Chapter6.cpp
@@ -0,0 +1,38 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+//Color Detection
+
+Mat imgHSV,mask;
+int hmin = 0, smin = 110, vmin = 153;
+int hmax = 19, smax = 240, vmax = 255;
+
+void main() {
+	string path = "resources/tesla.jpg";
+	Mat img = imread(path);
+	cvtColor(img, imgHSV, COLOR_BGR2HSV);
+
+	namedWindow("Trackbars", (640, 200));
+	createTrackbar("Hue Min", "Trackbars", &hmin, 179);
+	createTrackbar("Hue Max", "Trackbars", &hmax, 179);
+	createTrackbar("Sat Min", "Trackbars", &smin, 255);
+	createTrackbar("Sat Max", "Trackbars", &smax, 255);
+	createTrackbar("Val Min", "Trackbars", &vmin, 255);
+	createTrackbar("Val Max", "Trackbars", &vmax, 255);
+
+	while (true) {
+		Scalar lower(hmin, smin, vmin);
+		Scalar upper(hmax, smax, vmax);
+		inRange(imgHSV, lower, upper, mask);
+
+		imshow("Image", img);
+		imshow("Image HSV", imgHSV);
+		imshow("Image Mask", mask);
+		waitKey(1);
+	}
+}
