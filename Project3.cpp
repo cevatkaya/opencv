76ca2f1c6276a6bc70a3dd6a3d674d4d0cb9bf6d
Cevat Kaya <48681093+cevatkaya@users.noreply.github.com>
2023-01-09 19:33:44 +0300

    Add files via upload

---
+++ Project3.cpp
@@ -0,0 +1,43 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+/* 
+	Project 3 - License Plate Detection
+*/
+
+void main() {
+
+	VideoCapture cap(0); //you have one cam select 0, if you have more cam select 1
+	Mat	img;
+	CascadeClassifier plateCascade;
+	plateCascade.load("resources/haarcascade_russian_plate_number.xml");
+
+	if (plateCascade.empty()) {
+		cout << "XML file not loaded" << endl;
+	}
+
+	vector<Rect> plates;
+
+	while (true) {
+		cap.read(img);
+		plateCascade.detectMultiScale(img, plates, 1.1, 10);
+
+		for (int i = 0;i < plates.size();i++)
+		{
+			Mat imgCrop = img(plates[i]);
+			//imshow(to_string(i), imgCrop);
+
+			imwrite("resources/Plates/" + to_string(i) + ".png", imgCrop);
+			rectangle(img, plates[i].tl(), plates[i].br(), Scalar(255, 0, 255), 3);
+		}
+
+		imshow("Image", img);
+		waitKey(1);
+	}
+}
