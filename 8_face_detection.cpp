a6d4ca8c57a4b2754af35494412e8a5a86232d43
Cevat Kaya <48681093+cevatkaya@users.noreply.github.com>
2023-01-18 20:11:17 +0300

    Update

--- 8_face_detection.cpp
+++ 8_face_detection.cpp
@@ -7,26 +7,28 @@
 using namespace cv;
 using namespace std;
 
-// Face Detection
+/* 
+	Face Detection
+*/
 
 void main() {
 
 	string path = "resources/iam.jpg";
-	Mat img = imread(path);  //matrix data type
+	Mat img = imread(path);  // matrix data type
 	
 	CascadeClassifier faceCascade;
 	faceCascade.load("resources/haarcascade_frontalface_default.xml");
 	
-	if (faceCascade.empty()) 
-	{ 
+	if (faceCascade.empty()) { 
+		
 		cout << "XML file not loaded" << endl; 
 	}
 
 	vector<Rect> faces;
 	faceCascade.detectMultiScale(img, faces, 1.1, 10);
 
-	for (int i = 0;i < faces.size();i++) 
-	{
+	for (int i = 0;i < faces.size();i++) {
+		
 		rectangle(img, faces[i].tl(), faces[i].br(), Scalar(255, 0, 255), 3);
 	}
 
